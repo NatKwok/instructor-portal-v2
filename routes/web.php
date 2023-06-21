c2d12e55ff58ac6e9c15a435b32284a3a4e76524
NatKwok <natkwok902@gmail.com>
2023-06-21 09:25:53 -0400

    initial start

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -35,26 +33,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::group(['middleware' => 'auth', 'prefix' => 'subjects'], function () {
-    Route::get('/', [SubjectController::class, 'index'])->name('subject.index');
-    Route::get('/create', [SubjectController::class, 'create'])->name('subject.create');
-    Route::post('/store', [SubjectController::class, 'store'])->name('subject.store');
-    Route::get('/edit/{subject}', [SubjectController::class, 'edit'])->name('subject.edit');
-    Route::patch('/update/{subject}', [SubjectController::class, 'update'])->name('subject.update');
-    Route::patch('/delete', [SubjectController::class, 'destroy'])->name('subject.destroy');
-
-});
-
-Route::group(['middleware' => 'auth', 'prefix' => 'books'], function () {
-    Route::get('/', [BookController::class, 'index'])->name('book.index');
-    Route::get('/create', [BookController::class, 'create'])->name('book.create');
-    Route::post('/store', [BookController::class, 'store'])->name('book.store');
-    Route::get('/edit/{book}', [BookController::class, 'edit'])->name('book.edit');
-    Route::patch('/update/{book}', [BookController::class, 'update'])->name('book.update');
-    Route::patch('/delete', [BookController::class, 'destroy'])->name('book.destroy');
-
 });
 
 require __DIR__.'/auth.php';
